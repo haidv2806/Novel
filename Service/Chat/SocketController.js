a2d4d02f398fdc57c0d1f5b24f920798940cb40b
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-10-10 15:22:07 +0700

    hoàn thiện nhận dữ liệu từ socket

--- Service/Chat/SocketController.js
+++ Service/Chat/SocketController.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from 'http';
 import { Server } from "socket.io";
-// import { server } from "../../index.js";
+import passport from "../../Model/passport.js";
 
 const app = express();
 
@@ -19,15 +19,48 @@ const io = new Server(server, {
 // Định nghĩa một namespace cụ thể
 const newsSocketNamespace = io.of('/Socket');
 
+// kiểm tra token
+// Middleware để sử dụng passport xác thực access token
+newsSocketNamespace.use((socket, next) => {
+  const token = socket.handshake.auth.accessToken || socket.handshake.query.accessToken; // Lấy token từ query hoặc auth
+
+  if (!token) {
+    return next(new Error('Authentication error: Token is required'));
+  }
+
+  // Tạo một request ảo để dùng cho passport
+  const req = {
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  };
+
+  // Passport-JWT xử lý xác thực
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) {
+      return next(new Error('Authentication error: Invalid token'));
+    }
+
+    socket.user = user;  // Lưu thông tin người dùng đã xác thực
+    next();              // Tiếp tục kết nối nếu token hợp lệ
+  })(req);
+});
+
+
 // Sự kiện khi có kết nối socket mới
 newsSocketNamespace.on('connection', (socket) => {
-  console.log("User connected to /Socket:", socket.id);
+  console.log("User connected:", socket.user.user_name);
 
   socket.on('send_message', (data) => {
     console.log(data);
-    
+
     // Emit tin nhắn tới tất cả kết nối trong namespace
-    newsSocketNamespace.emit("receive_message", data);
+    newsSocketNamespace.emit("receive_message", {
+      id: socket.id,
+      userImage: "https://fastly.picsum.photos/id/99/536/354.jpg?hmac=TAjl9K8MlAuXK5gnvATXTmOHkoN7bOjyqto5Qhxn1Cg",
+      userName: "Olivia White",
+      userComment: data.message,
+    });
   });
 
   // Xử lý khi socket ngắt kết nối
