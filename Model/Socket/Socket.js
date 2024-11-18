8c9f8f514056d1ccb1130b5e9d35158398bb2597
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-11-18 15:25:31 +0700

    điều tiết giá trị trả về khi join vào Reply room

--- Model/Socket/Socket.js
+++ Model/Socket/Socket.js
@@ -1,6 +1,38 @@
 import db from '../../Service/database.js'
 
 class Socket {
+    MainChat
+    // socket_id
+    // room_id
+    // user_id
+    // avatar
+    // content
+    // timestamp
+    Reply
+
+    constructor (){
+        this.Reply =[]
+    }
+
+    // lấy dữ liệu cho 1 group chat của reply
+    async init(socket_id){
+        const mainChat = await Socket.getChat(socket_id)
+        const reply = await Socket.getChatInRoom(socket_id, 1)
+        console.log(reply);
+        
+
+        if (mainChat) {
+            this.MainChat = mainChat
+            // this.socket_id = mainChat.socket_id
+            // this.room_id = mainChat.room_id
+            // this.user_id = mainChat.user_id
+            // this.avatar = mainChat.avatar
+            // this.content = mainChat.content
+            // this.timestamp = mainChat.timestamp
+            this.Reply = reply
+        }
+    }
+
     static async addNewChat(room_id, user_id, content) {
         const query = `
             INSERT INTO socket (room_id, user_id, content)
@@ -16,6 +48,24 @@ class Socket {
         }
     }
 
+    static async getChat(socket_id){
+        const query = `
+            SELECT socket_id, room_id, socket.user_id, user_name, avatar, content, timestamp
+            FROM socket
+                INNER JOIN users ON socket.user_id = users.user_id
+            WHERE socket_id = $1
+        `
+
+        const number = parseInt(socket_id.replace(/\D/g, ''), 10);
+        try {
+            const result = await db.query(query, [number])
+            return result.rows[0]
+        } catch (err) {
+            console.error('Error take a chat by socket id:', err);
+            throw err;
+        }
+    }
+
     static async addNewReply(socket_id, user_id, content) {
         const query = `
             INSERT INTO socket_replies (socket_id, user_id, content)
@@ -27,6 +77,23 @@ class Socket {
             return result.rows[0]
         } catch (err) {
             console.error('Error adding new reply in to database:', err);
+            throw err;
+        }
+    }
+    
+    static async getAllReplyFromSocketId(socket_id, page){
+        const query = `
+            SELECT *
+            FROM socket_replies
+            WHERE socket_id = $1
+            ORDER BY timestamp DESC
+            LIMIT 10 OFFSET $2
+        `
+        try {
+            const result = await db.query(query, [socket_id, (page * 10) - 10])
+            return result.rows
+        } catch (error) {
+            console.error(`Error get all chat in socket_id ${socket_id} room:`, err);
             throw err;
         }
     }
@@ -57,9 +124,11 @@ class Socket {
         `
         try {
             const result = await db.query(query, [room_id, (page * 10) - 10])
+            console.log(room_id);
+            
             return result.rows
         } catch (err) {
-            console.error('Error adding new like to a socket:', err);
+            console.error(`Error get chat in room ${room_id} :`, err);
             throw err;
         }
     }
--- Service/Socket/SocketController.js
+++ Service/Socket/SocketController.js
@@ -63,40 +63,60 @@ newsSocketNamespace.on('connection', (socket) => {
 
   // Xử lý sự kiện join_room
   socket.on('join_room', async (roomID) => {
-    try {
-      socket.join(roomID); // Tham gia vào phòng cụ thể
-      console.log(`User ${socket.user.user_name} joined room ${roomID}`);
+    if (roomID.startsWith('Reply')) {
+      try {
+        socket.join(roomID); // Tham gia vào phòng cụ thể
+        console.log(`User ${socket.user.user_name} joined room ${roomID}`);
 
-      // Lấy dữ liệu chat trong phòng từ cơ sở dữ liệu
-      const result = await Socket.getChatInRoom(roomID, 1);
+        // Lấy dữ liệu chat trong phòng từ cơ sở dữ liệu
+        const socketdata = new Socket()
+        await socketdata.init(roomID)
+        console.log(socketdata);
+        
 
-      // Gửi dữ liệu chat về cho người dùng vừa tham gia phòng
-      socket.emit('room_data', result);  // Trả dữ liệu chat cho chính socket này
-    } catch (err) {
-      console.error('Error fetching chat data:', err);
-      socket.emit('error', { message: 'Không thể lấy dữ liệu chat cho phòng này' });
+        // Gửi dữ liệu chat về cho người dùng vừa tham gia phòng
+        socket.emit('room_data', socketdata);  // Trả dữ liệu chat cho chính socket này
+      } catch (err) {
+        console.error('Error fetching reply data:', err);
+        socket.emit('error', { message: 'Không thể lấy dữ liệu chat cho phòng reply này' });
+      }
+
+    } else {
+      try {
+        socket.join(roomID); // Tham gia vào phòng cụ thể
+        console.log(`User ${socket.user.user_name} joined room ${roomID}`);
+
+        // Lấy dữ liệu chat trong phòng từ cơ sở dữ liệu
+        const result = await Socket.getChatInRoom(roomID, 1);
+        
+        // Gửi dữ liệu chat về cho người dùng vừa tham gia phòng
+        socket.emit('room_data', {MainChat: result});  // Trả dữ liệu chat cho chính socket này
+      } catch (err) {
+        console.error('Error fetching chat data:', err);
+        socket.emit('error', { message: 'Không thể lấy dữ liệu chat cho phòng này' });
+      }
     }
   });
 
   // Xử lý khi nhận tin nhắn từ client
   socket.on('send_message', async ({ message, roomID }) => {
-    try {
-      // lưu và cơ sở dữ liệu 
-      const chat = await Socket.addNewChat(roomID, socket.user.user_id, message)
-      // Gửi tin nhắn tới tất cả các client trong cùng một phòng
-      newsSocketNamespace.emit("receive_message", {
-        socket_id: chat.socket_id,
-        room_id: chat.room_id,
-        user_id: chat.user_id,
-        user_name: socket.user.user_name,
-        avatar: socket.user.avatar,
-        content: chat.content,
-        timestamp: chat.timestamp
-      });
-    } catch (err) {
-      console.error('Error save chat data:', err);
-      socket.emit('error', { message: 'không thể lưu trữ dữ liệu' });
-    }
+      try {
+        // lưu và cơ sở dữ liệu 
+        const chat = await Socket.addNewChat(roomID, socket.user.user_id, message)
+        // Gửi tin nhắn tới tất cả các client trong cùng một phòng
+        newsSocketNamespace.emit("receive_message", {
+          socket_id: chat.socket_id,
+          room_id: chat.room_id,
+          user_id: chat.user_id,
+          user_name: socket.user.user_name,
+          avatar: socket.user.avatar,
+          content: chat.content,
+          timestamp: chat.timestamp
+        });
+      } catch (err) {
+        console.error('Error save chat data:', err);
+        socket.emit('error', { message: 'không thể lưu trữ dữ liệu' });
+      }
   });
 
   // Xử lý khi socket ngắt kết nối
